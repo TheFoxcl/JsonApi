5d3473b559ea95eccde691dca53a881ab65e802a
TheFoxcl <99927293+TheFoxcl@users.noreply.github.com>
2022-04-21 21:24:25 -0500

    Delete consumo.js

--- consumo.js
+++
@@ -1,9 +0,0 @@
-const API_URL = "http://127.0.0.1:8000/api/clientes/";
-const HTMLResponse = document.querySelector('#app');
-
-fetch(`${API_URL}/clientes`)
-    .then((response) => response.json())
-    .then((clientes) =>{
-        const tpl = clientes.map((clientes) => `<li>${clientes.identificación} ${clientes.númeroDeProductosVendidos}</li>`);
-        HTMLResponse.innerHTML = `<ul>${tpl}</ul>`
-    })
